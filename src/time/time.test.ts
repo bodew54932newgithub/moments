b1f8cc0ffbafb5a89d98c2150055835b5e5bd98f
Frederic Espiau <frederic.espiau@gmail.com>
2021-08-01 17:38:13 +0200

    feat: handle time being negative

--- src/time/time.test.ts
+++ src/time/time.test.ts
@@ -1,4 +1,4 @@
-import { Time } from "./time";
+import { Time, TimeNegativeError } from "./time";
 
 describe("Time", () => {
   test("when time is a valid number of seconds", () => {
@@ -9,5 +9,10 @@ describe("Time", () => {
   test("when time is a valid string of seconds", () => {
     const input = "42";
     expect(Time(input)).toStrictEqual(42);
+  });
+
+  test("when time is a negative number of seconds, it should fail", () => {
+    const input = -1;
+    expect(() => Time(input)).toThrowError(new TimeNegativeError(input));
   });
 });
--- src/time/time.ts
+++ src/time/time.ts
@@ -1,2 +1,14 @@
-export const Time = (input) =>
-  typeof input === "number" ? input : Number.parseInt(input, 10);
+export class TimeNegativeError extends Error {
+  constructor(input) {
+    super(`Time cannot be negative: ${input}`);
+  }
+}
+export const Time = (input) => {
+  const value = typeof input === "number" ? input : Number.parseInt(input, 10);
+
+  if (value < 0) {
+    throw new TimeNegativeError(input);
+  }
+
+  return value;
+};
