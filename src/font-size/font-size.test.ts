0f13ce7c3526b6c385bafb85c8aa2a3f694375de
Frederic Espiau <frederic.espiau@gmail.com>
2021-08-01 16:55:19 +0200

    feat: handle empty font size

--- src/font-size/font-size.test.ts
+++ src/font-size/font-size.test.ts
@@ -1,8 +1,13 @@
-import { FontSize } from "./font-size";
+import { FontSize, FontSizeEmpty } from "./font-size";
 
 describe("FontSize", () => {
   test("when the font size is valid", () => {
     const input = 42;
-    expect(FontSize(42)).toStrictEqual(input);
+    expect(FontSize(input)).toStrictEqual(input);
+  });
+
+  test("when the font size is empty, it should fail", () => {
+    const input = "";
+    expect(() => FontSize(input)).toThrowError(new FontSizeEmpty());
   });
 });
--- src/font-size/font-size.ts
+++ src/font-size/font-size.ts
@@ -1,1 +1,13 @@
-export const FontSize = (input) => input;
+export class FontSizeEmpty extends Error {
+  constructor() {
+    super("Font size cannot be empty");
+  }
+}
+
+export const FontSize = (input) => {
+  if (!["string", "number"].includes(typeof input) || input === "") {
+    throw new FontSizeEmpty();
+  }
+
+  return input;
+};
