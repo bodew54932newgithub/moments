833a761f4cc6a2e1ca06f9444ed39bed2ed526ea
Frederic Espiau <7319147+FredericEspiau@users.noreply.github.com>
2021-08-01 16:06:33 +0200

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	moduleFileExtensions: ['jsx', 'js', 'json', 'ts', 'tsx'],
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+	transform: {
+		'^.+\\.(t|j)s$': 'ts-jest'
+	},
+};
