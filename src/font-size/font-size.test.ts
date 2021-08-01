df8b0caa5fa6c8055ca16aa065da0a7eeed20596
Frederic Espiau <frederic.espiau@gmail.com>
2021-08-01 16:58:01 +0200

    feat: handle font size being too small

--- src/font-size/font-size.test.ts
+++ src/font-size/font-size.test.ts
@@ -1,13 +1,30 @@
-import { FontSize, FontSizeEmpty } from "./font-size";
+import {
+  FontSize,
+  FontSizeEmptyError,
+  FontSizeTooSmallError,
+  MIN,
+} from "./font-size";
 
 describe("FontSize", () => {
-  test("when the font size is valid", () => {
+  test("when the font size is a valid number", () => {
     const input = 42;
     expect(FontSize(input)).toStrictEqual(input);
   });
 
+  test("when the font size is a valid string, it should be converted to number", () => {
+    const input = "42";
+    expect(FontSize(input)).toStrictEqual(42);
+  });
+
   test("when the font size is empty, it should fail", () => {
     const input = "";
-    expect(() => FontSize(input)).toThrowError(new FontSizeEmpty());
+    expect(() => FontSize(input)).toThrowError(new FontSizeEmptyError());
+  });
+
+  test(`when the font size is under ${MIN}, it should fail`, () => {
+    const input = 0;
+    expect(() => FontSize(input)).toThrowError(
+      new FontSizeTooSmallError(input)
+    );
   });
 });
--- src/font-size/font-size.ts
+++ src/font-size/font-size.ts
@@ -1,13 +1,27 @@
-export class FontSizeEmpty extends Error {
+export class FontSizeEmptyError extends Error {
   constructor() {
     super("Font size cannot be empty");
   }
 }
 
+export const MIN = 1;
+
+export class FontSizeTooSmallError extends Error {
+  constructor(value) {
+    super(`Font size cannot be under ${MIN}: ${value}`);
+  }
+}
+
 export const FontSize = (input) => {
   if (!["string", "number"].includes(typeof input) || input === "") {
-    throw new FontSizeEmpty();
+    throw new FontSizeEmptyError();
   }
 
-  return input;
+  const value = Number.parseInt(input, 10);
+
+  if (value < MIN) {
+    throw new FontSizeTooSmallError(input);
+  }
+
+  return value;
 };
